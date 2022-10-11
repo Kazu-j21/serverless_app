57557cb60bf5f7380d762e2ff1c61f193df19666
Kazu-j21 <nishiadgjmptw1021@gmail.com>
2022-10-11 16:14:18 +0900

    Set cognito config

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_OoPdI0Vjt', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '7iu5g3io9iokqsmpia44dff23r', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'us-east-1' // e.g. us-east-2
+        userPoolId: 'ap-northeast-1_nrac32uef', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2jcakike6krqjuqd1la95pmcrl', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-northeast-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: 'https://py7cvw86ib.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
